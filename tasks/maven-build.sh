96e983689aea8ebc2236b438ee6aa2c4143c9989
Mateusz Matocha <mateusz.matocha@grapeup.com>
2019-03-08 11:42:37 +0100

    updated script

--- tasks/maven-build.sh
+++ tasks/maven-build.sh
@@ -6,7 +6,6 @@ mvn clean install
 
 NAME=`ls target/*.jar | cut -d '/' -f 2 | rev | cut -c 5- | rev`-`date +%s%N`
 
-mv manifest.yml ../artifacts/
 mv target/*.jar ../artifacts/
 tar cvf $NAME.tar -C ../artifacts/ .
 mv $NAME.tar ../artifacts/
