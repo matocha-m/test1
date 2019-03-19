bd8d57851f1792d12d43be2abf582098bcd5ede7
Mateusz Matocha <mateusz.matocha@grapeup.com>
2019-03-19 15:49:28 +0100

    added iot edge client

--- tasks/maven-build.sh
+++ tasks/maven-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
-ls -la
+RUN mvn org.apache.maven.plugins:maven-dependency-plugin:3.1.1:get -Dartifact=com.microsoft.azure.sdk.iot:iot-device-client:1.14.0 -DrepoUrl=https://repo.maven.apache.org/maven2/
+
 cd source-code/modules/SampleModule
-ls -la
 mvn clean install
 
 NAME=`ls target/*.jar | cut -d '/' -f 2 | tail -n 1`
