13387143624a9ad405b05199d450f7db955913f5
Mateusz Matocha <mateusz.matocha@grapeup.com>
2019-03-19 15:51:42 +0100

    fixed script

--- tasks/maven-build.sh
+++ tasks/maven-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
-RUN mvn org.apache.maven.plugins:maven-dependency-plugin:3.1.1:get -Dartifact=com.microsoft.azure.sdk.iot:iot-device-client:1.14.0 -DrepoUrl=https://repo.maven.apache.org/maven2/
+mvn org.apache.maven.plugins:maven-dependency-plugin:3.1.1:get -Dartifact=com.microsoft.azure.sdk.iot:iot-device-client:1.14.0 -DrepoUrl=https://repo.maven.apache.org/maven2/
 
 cd source-code/modules/SampleModule
 mvn clean install
