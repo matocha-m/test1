78e4412db2a36cac930d11ca5eb89b849456c4a9
Mateusz Matocha <mateusz.matocha@grapeup.com>
2019-03-19 13:40:38 +0100

    fixed script

--- tasks/maven-build.sh
+++ tasks/maven-build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -ex
 
-cd source-code
+cd source-code/modules/SampleModule
 
 mvn clean install
 
 NAME=`ls target/*.jar | cut -d '/' -f 2 | rev | cut -c 5-`
 
-mv target/*.jar ../artifacts/
-tar cvf $NAME.tar -C ../artifacts/ .
-mv $NAME.tar ../artifacts/
+mv target/*.jar ../../../artifacts/
+tar cvf $NAME.tar -C ../../../artifacts/ .
+mv $NAME.tar ../../../artifacts/
