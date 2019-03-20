d4c4bb9b1e1d7d621a21834a85e288463380fcbd
Mateusz Matocha <mateusz.matocha@grapeup.com>
2019-03-20 14:24:50 +0100

    testing

--- tasks/deploy_solution.sh
+++ tasks/deploy_solution.sh
@@ -2,5 +2,7 @@
 
 az login --service-principal -u $USERNAME -p $PASSWORD --tenant $AZ_AD
 az extension add --name azure-cli-iot-ext
-VERSION=v`cat source-code/tag.txt`
-az iot edge deployment create -d test_$VERSION -n mm-iot-hub-test --content source-code/config/deployment.amd64.json --target-condition deviceId='$DEVICE_ID'
+VERSION=v$(cat source-code/tag.txt)
+CONDITION="\"deviceId='${DEVICE_ID}'\""
+echo $CONDITION
+az iot edge deployment create -d test_$VERSION -n mm-iot-hub-test --content source-code/config/deployment.amd64.json --target-condition '\"deviceId='$DEVICE_ID'\"'
