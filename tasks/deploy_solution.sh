c95a965af3c327a232831e245557c83c6fe64a88
Mateusz Matocha <mateusz.matocha@grapeup.com>
2019-03-20 13:04:23 +0100

    set up login to az

--- tasks/deploy_solution.sh
+++ tasks/deploy_solution.sh
@@ -1,4 +1,4 @@
-az login -u $USERNAME -p $PASSWORD
+az login --service-principal -u $USERNAME -p $PASSWORD --tenant $AZ_AD
 az extension add --name azure-cli-iot-ext
 VERSION=v`cat source-code/tag.txt`
 az iot edge deployment create -d test_$VERSION -n mm-iot-hub-test --content source-code/config/deployment.amd64.json --target-condition="deviceId=''$DEVICE_ID''"
