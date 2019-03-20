64590d6608a6b879d6d0e9c7f053b791ec653887
Mateusz Matocha <mateusz.matocha@grapeup.com>
2019-03-20 11:38:31 +0100

    updated pipelines

--- tasks/maven-build.sh
+++ tasks/maven-build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -ex
-#mvn org.apache.maven.plugins:maven-dependency-plugin:3.1.1:get -Dartifact=com.microsoft.azure.sdk.iot:iot-device-client:1.14.0 -DrepoUrl=https://repo.maven.apache.org/maven2/
 
-cd source-code/modules/SampleModule
+cd source-code/modules/$MODULE_NAME
 mvn clean install
 
 NAME=`ls target/*.jar | cut -d '/' -f 2 | grep 'with-deps.jar$'`
