282b41f37baffbdf3411c59fb05299001543ab0d
Mateusz Matocha <mateusz.matocha@grapeup.com>
2019-03-21 10:46:36 +0100

    updated version

--- tasks/deploy_solution.sh
+++ tasks/deploy_solution.sh
@@ -2,6 +2,6 @@
 
 az login --service-principal -u $USERNAME -p $PASSWORD --tenant $AZ_AD
 az extension add --name azure-cli-iot-ext
-VERSION=v$(cat source-code/tag.txt)
+VERSION=v$(cat source-code/tag.txt | tr . _)
 
 az iot edge deployment create -d test_$VERSION -n mm-iot-hub-test --content source-code/config/deployment.amd64.json --target-condition "deviceId='mm_test_dev'"
