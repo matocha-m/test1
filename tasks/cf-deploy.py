454a018b0cc1fb64fff3a0baa3a2cc0f5485c5aa
Mateusz Matocha <mateusz.matocha@grapeup.com>
2019-07-04 08:12:30 +0200

    json check

--- tasks/cf-deploy.py
+++ tasks/cf-deploy.py
@@ -1,13 +1,18 @@
 #!/usr/local/bin/python
 
-import yaml
+# import yaml
 import os
+import json
 
 env_variables = os.environ['environment_variables']
 print(env_variables)
 
-with open("example.yaml", 'r') as stream:
-    try:
-        print(yaml.safe_load(stream))
-    except yaml.YAMLError as exc:
-        print(exc)
+
+d = json.loads(env_variables)
+print (d['SPRING_CLOUD_CONFIG_USERNAME'])
+
+# with open("example.yaml", 'r') as stream:
+#     try:
+#         print(yaml.safe_load(stream))
+#     except yaml.YAMLError as exc:
+#         print(exc)
